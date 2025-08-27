fd1bd1ee6d0de381284e4f6cf6c290060db9176a
Jamie Nevin <jmenvn@gmail.com>
2025-08-27 04:32:44 +0100

    exercise 2.8

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -5,40 +5,49 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       id: '1',
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: '39-44-53223523'
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName,
-      id: String(persons.length + 1)
+
+    if (!newName.trim()) {
+      return window.alert('No name entered')
     }
 
-    if (persons.some(person => person.name === personObject.name)) {
-      window.alert(`${personObject.name} is already added to phonebook`)
-      return
+    if (!newNumber.trim()) {
+      return window.alert('No number entered')
     }
-    setPersons(persons.concat(personObject))
+
+    if (persons.some(person => person.name === newName)) {
+      return window.alert(`${newName} is already added to phonebook`)
+    }
+
+    setPersons([...persons, {
+      id: String(persons.length + 1),
+      name: newName,
+      number: newNumber
+    }])
+
     setNewName('')
+    setNewNumber('')
   }
 
   const handleAddName = (event) => setNewName(event.target.value)
+
+  const handleAddNumber = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleAddName}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleAddName}/></div>
+        <div>number: <input value={newNumber} onChange={handleAddNumber}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <div>
