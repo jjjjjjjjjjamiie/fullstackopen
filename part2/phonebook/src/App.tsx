c11379c3d4de0dcb26336703ca4686271a3f3711
Jamie Nevin <jmenvn@gmail.com>
2025-08-27 04:51:53 +0100

    exercise 2.9

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -3,14 +3,14 @@ import Person from "./components/Person.tsx";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      id: '1',
-      name: 'Arto Hellas',
-      number: '39-44-53223523'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -28,7 +28,7 @@ const App = () => {
     }
 
     setPersons([...persons, {
-      id: String(persons.length + 1),
+      id: persons.length + 1,
       name: newName,
       number: newNumber
     }])
@@ -41,9 +41,17 @@ const App = () => {
 
   const handleAddNumber = (event) => setNewNumber(event.target.value)
 
+  const handleNameFilter = (event) => setNameFilter(event.target.value)
+
+  const namesToShow = persons.filter(p =>
+    p.name.toLowerCase().includes(nameFilter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleNameFilter}/></div>
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>name: <input value={newName} onChange={handleAddName}/></div>
         <div>number: <input value={newNumber} onChange={handleAddNumber}/></div>
@@ -51,7 +59,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
+        {namesToShow.map(person =>
           <Person key={person.id} person={person}/>
         )}
       </div>
