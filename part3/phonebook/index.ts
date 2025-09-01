5157d07933de935a4b8c3fe9e8ceada90873a178
Jamie Nevin <jmenvn@gmail.com>
2025-09-01 18:11:01 +0100

    exercise 3.3

--- part3/phonebook/index.ts
+++ part3/phonebook/index.ts
@@ -29,6 +29,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+  person ?
+    response.json(person) :
+    response.status(404).end()
+})
+
 app.get('/info', (request, response) => {
   let now = new Date().toUTCString();
   response.send(`
