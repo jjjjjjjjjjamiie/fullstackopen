5bf8df8cf12c588682d93131ff46fec752c83e8f
Jamie Nevin <jmenvn@gmail.com>
2025-09-01 20:52:41 +0100

    updated backend port

--- part3/phonebook/index.ts
+++ part3/phonebook/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import * as process from "node:process";
 
 const app = express()
 app.use(express.json())
@@ -86,7 +87,7 @@ app.get('/info', (request, response) => {
 `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
