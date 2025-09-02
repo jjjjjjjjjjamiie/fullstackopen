72604c33d5de3500a60956f2b98c7625ee325dbb
Jamie Nevin <jmenvn@gmail.com>
2025-09-02 17:24:00 +0100

    exercise 3.12

---
+++ part3/phonebook/mongo.ts
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://jjamiie:${password}@bigboy.jtdarig.mongodb.net/personApp?retryWrites=true&w=majority&appName=BigBoy`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: personName,
+  number: personNumber,
+})
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length === 4) {
+  person.save().then(result => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
