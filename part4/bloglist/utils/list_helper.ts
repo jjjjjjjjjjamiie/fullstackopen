7ccb281e02a65686bc18c65fee802a1ca669da76
Jamie Nevin <jmenvn@gmail.com>
2025-09-23 01:33:30 +0100

    exercise 4.5

--- part4/bloglist/utils/list_helper.ts
+++ part4/bloglist/utils/list_helper.ts
@@ -4,4 +4,10 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-export default {dummy, totalLikes}
+const favouriteBlogs = (blogs) => {
+  let reduce = blogs.reduce((mostLikes, blog) => {
+    return blog.likes > mostLikes ? blog.likes : mostLikes;
+  }, 0);
+  return blogs.filter(blog => blog.likes === reduce)[0] ?? []
+}
+export default {dummy, totalLikes, favouriteBlogs}
