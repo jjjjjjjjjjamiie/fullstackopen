346902b125bee338ed98c0a74d19757c4a426050
Jamie Nevin <jmenvn@gmail.com>
2025-10-23 23:24:23 +0100

    exercise 4.17

--- part4/bloglist/controllers/blogs.ts
+++ part4/bloglist/controllers/blogs.ts
@@ -1,16 +1,31 @@
 import Blog from '../models/blog'
 import {Router} from 'express'
+import User from '../models/user'
 
 const blogsRouter = Router()
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const users = await User.find({})
+  const user = users[0]
+
+  if (!user) {
+    return response.status(400).json({error: 'User ID missing or invalid'})
+  }
+
+  const blog = new Blog({
+    ...request.body,
+    user: user._id,
+  })
+
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+
   response.status(201).json(result)
 })
 
